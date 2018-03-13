be8f8ca2f87a98c98daf638cc75dc13c3678f193
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-03-13 09:22:11 -0400

    d

--- Lab 4.1 Cards/src/Card.java
+++ Lab 4.1 Cards/src/Card.java
@@ -1,4 +1,11 @@
 
 public class Card {
-
+	private String rank;
+	private String suit;
+	private int value;
+	public Card(String rank, String suit, int value) {
+		this.rank = rank;
+		this.suit = suit;
+		this.value = value;
+	}
 }
