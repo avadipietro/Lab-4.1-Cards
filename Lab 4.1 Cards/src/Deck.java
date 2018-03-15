7d2f1942f6f2ed2e216005dd39b4c2024972609a
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-03-15 09:21:19 -0400

    fgfgffgfg

--- Lab 4.1 Cards/src/Deck.java
+++ Lab 4.1 Cards/src/Deck.java
@@ -4,6 +4,22 @@ public class Deck {
 	private ArrayList<Card> unDealt;
 	private ArrayList<Card> dealt;
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		
+		this.unDealt = new ArrayList<Card>();
+		this.dealt = new ArrayList<Card>();
+		for (int i = 0; i < ranks.length; i++) {
+			for (int j = 0; j < suits.length; j++) {
+				unDealt.add(new Card(ranks[i], suits[j], values[i]));
+			}
+		}
+	}
+	public boolean isEmpty() {
+		return unDealt.size() == 0;
+	}
+	public int size() {
+		return unDealt.size();
+	}
+	public Card deal() {
+		unDealt[i];
+		unDealt.remove(unDealt.size() - 1);
 	}
 }
